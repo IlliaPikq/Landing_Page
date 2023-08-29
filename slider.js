77124d2bd19225fc96ee49e66639287becfa4fd6
Illia <illiapikhotskyi@gmail.com>
2023-08-29 03:55:26 +0200

    clean up

--- slider.js
+++ slider.js
@@ -1,25 +1,25 @@
-const slider = document.querySelector(".slider");
 
-let pressed = false,
-	startX;
-
-slider.addEventListener("mousedown", (e) => {
-	pressed = true;
-	startX = e.clientX;
-	e.target.style.cursor = "grabbing";
-	e.preventDefault();
-});
-
-window.addEventListener("mouseup", () => {
-	pressed = false;
-});
-
-slider.addEventListener("mouseleave", () => {
-	pressed = false;
-	e.target.cursor = "pointer";
-});
-
-slider.addEventListener("mousemove", function (e) {
-    if (!pressed) return;
-	this.scrollLeft += 5 * (startX - e.clientX);	
-});
+if(screen && screen.width == 800) {
+	const slider = document.querySelector(".slider");
+	let pressed = false,
+		startX;
+	slider.addEventListener("mousedown", (e) => {
+		pressed = true;
+		startX = e.clientX;
+		e.target.style.cursor = "grabbing";
+		e.preventDefault();
+	});
+	
+	window.addEventListener("mouseup", () => {
+		pressed = false;
+	});
+	
+	slider.addEventListener("mouseleave", () => {
+		pressed = false;
+	});
+	
+	slider.addEventListener("mousemove", function (e) {
+		if (!pressed) return;
+		this.scrollLeft += 5 * (startX - e.clientX);	
+	});
+}
